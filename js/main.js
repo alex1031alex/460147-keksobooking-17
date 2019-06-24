b74162048dc2228d26d462dbccf668bf9f9c8f51
Alex <alex1031alex@yandex.ru>
2019-06-24 22:59:27 +0300

    Объявляет функцию включения/отключения элементов управления форм. Добавляет метке обработчик клика, который переводит страницу в активное состояние

--- js/main.js
+++ js/main.js
@@ -6,9 +6,16 @@ var MIN_Y = 130;
 var MAX_Y = 630;
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
+var MAIN_PIN_WIDTH = 65;
+var MAIN_PIN_HEIGHT = 65;
 var homeTypes = ['palace', 'flat', 'house', 'bungalo'];
 var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var mapPinList = document.querySelector('.map__pins');
+var map = document.querySelector('.map');
+var mapPin = document.querySelector('.map__pin--main');
+var adForm = document.querySelector('.ad-form');
+var filterForm = document.querySelector('.map__filters');
+var addressInput = adForm.querySelector('#address');
 
 var getRandomNumber = function (min, max) {
   var number = Math.floor(min + Math.random() * (max + 1 - min));
@@ -51,3 +58,39 @@ var fillNoticeList = function (list, notices) {
 
   list.appendChild(fragment);
 };
+
+var switchFormControls = function (form, isDisabled) {
+  var inputs = form.querySelectorAll('input');
+  var selects = form.querySelectorAll('select');
+  var textareas = form.querySelectorAll('textarea');
+  var submit = form.querySelector('[type="submit"]');
+
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].disabled = isDisabled;
+  }
+
+  for (i = 0; i < selects.length; i++) {
+    selects[i].disabled = isDisabled;
+  }
+
+  for (i = 0; i < textareas.length; i++) {
+    textareas[i].disabled = isDisabled;
+  }
+
+  if (submit) {
+    submit.disabled = isDisabled;
+  }
+};
+
+switchFormControls(adForm, true);
+switchFormControls(filterForm, true);
+
+mapPin.addEventListener('click', function () {
+  map.classList.remove('map--faded');
+  adForm.classList.remove('ad-form--disabled');
+  switchFormControls(adForm, false);
+  switchFormControls(filterForm, false);
+  addressInput.value = Math.round((parseInt(mapPin.style.left) + MAIN_PIN_WIDTH / 2))
+  + ', ' + Math.round((parseInt(mapPin.style.top) + MAIN_PIN_HEIGHT / 2));
+  fillNoticeList(mapPinList, similarNotices);
+});
