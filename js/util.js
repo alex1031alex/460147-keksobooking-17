510d173caf8142974efd898c4c1d201316ed76e1
Alex <alex1031alex@yandex.ru>
2019-07-21 00:22:31 +0300

    Исправляет ошибку в функции обработчике ошибок

--- js/util.js
+++ js/util.js
@@ -5,7 +5,6 @@
   var escPressHandlerMaker = function (element) {
     var escPressHandler = function(evt) {
       if (evt.keyCode === ESC_KEY_CODE) {
-        console.log('gggg');
         element.remove();
         document.removeEventListener('keydown', escPressHandler);
       }
@@ -13,11 +12,10 @@
     return escPressHandler;
   };
 
-
   var errorHandler = function (message) {
     var errorTemplate = document.querySelector('#error').content.querySelector('.error');
     var errorModal = errorTemplate.cloneNode(true);
-    var errorButton = querySelector('.error__button');
+    var errorButton = errorModal.querySelector('.error__button');
     errorButton.textContent = message;
     document.querySelector('main').appendChild(errorModal);
     errorButton.addEventListener('click', function () {
